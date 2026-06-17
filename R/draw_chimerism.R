a9bfdedc3ec029ac8990791086873045104a16b5
felix-falk <felix.falk@proton.me>
2026-06-17 16:16:25 +0200

    Adjust relative heights in chimerism timeline plot layout for improved visual balance

--- R/draw_chimerism.R
+++ R/draw_chimerism.R
@@ -120,7 +120,7 @@ plot_chimerism_timeline <- function(processed, pat_id) {
     chimerism_plot_clean,
     events_plot,
     ncol = 1,
-    rel_heights = c(5, 1),
+    rel_heights = c(2, 2, 1),
     align = "v",
     axis = "tblr"
   )
