318638ef86f5974392a6e47250ddf9f17fc52351
felix-falk <felix.falk@proton.me>
2026-07-29 14:35:22 +0200

    Refactor events plot theme to simplify minor grid line handling

--- R/draw_clinical_course.R
+++ R/draw_clinical_course.R
@@ -454,9 +454,8 @@ draw_events_plot <- function(
     ggplot2::theme_minimal() +
     ggplot2::theme(
       legend.position = if (show_ciclosporin_legend) "right" else "none",
-      axis.text.y = ggplot2::element_text(size = 10),
-      panel.grid.minor.y = ggplot2::element_blank(),
-      panel.grid.minor.x = ggplot2::element_blank()
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 10)
     ) +
     ggplot2::scale_x_continuous(limits = x_range) +
     ggplot2::scale_y_continuous(
